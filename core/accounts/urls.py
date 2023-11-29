0fef45a69ca4c1c3a0d65308e84b1e0dcf03007d
Hosein <mohammadhosein5686@gmail.com>
2023-11-29 12:26:43 +0330

    added simple cach weather api for test

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -5,6 +5,7 @@ app_name = "accounts"
 
 urlpatterns = [
     path("send-email/", views.send_email, name="send-email"),
+    path("weather/", views.WeatherTestView, name="weather-test"),
     path("", include("django.contrib.auth.urls")),
     path("api/v1/", include("accounts.api.v1.urls")),
 ]
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,8 +1,18 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.cache import cache_page
 
 from .tasks import SendEmail
+import requests
 
 
 def send_email(request):
     SendEmail.delay()
     return HttpResponse("<h1>Email sent</h1>")
+
+
+@cache_page(60 * 20)
+def WeatherTestView(request):
+    response = requests.get(
+        "https://api.openweathermap.org/data/2.5/weather?lat=37.268219&lon=49.589123&appid=9be11538ceebf4c5d0ee0c5d97579881"
+    )
+    return JsonResponse(response.json())
